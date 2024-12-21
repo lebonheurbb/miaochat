463a1bd2f355ed3dc22942ecbe5450f2554a2778
lebonheurbb <122594742+lebonheurbb@users.noreply.github.com>
2024-12-21 15:22:13 +0800

    refactor: 重写 socket route 以适配 Next.js 13+

--- app/api/socket/route.ts
+++ app/api/socket/route.ts
@@ -1,41 +1,47 @@
+import { NextResponse } from 'next/server'
 import { Server as SocketServer } from 'socket.io'
-import { Server as NetServer } from 'http'
-import { NextApiRequest } from 'next'
-import { NextApiResponse } from 'next'
 
-// 使用新的路由配置格式
+declare global {
+  var io: SocketServer | undefined
+}
+
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 
-const initSocket = (req: NextApiRequest, res: NextApiResponse) => {
-  if (!res.socket.server.io) {
-    const httpServer: NetServer = res.socket.server as any
-    const io = new SocketServer(httpServer, {
-      path: '/api/socket',
-      addTrailingSlash: false,
-      cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-      },
+let io: SocketServer
+
+if (!global.io) {
+  io = new SocketServer({
+    path: '/api/socket',
+    addTrailingSlash: false,
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  })
+
+  io.on('connection', (socket) => {
+    console.log('Client connected')
+
+    socket.on('join-chat', (chatId) => {
+      console.log('Client joined chat:', chatId)
+      socket.join(chatId)
     })
 
-    io.on('connection', (socket) => {
-      console.log('Client connected')
-
-      socket.on('join-chat', (chatId) => {
-        console.log('Client joined chat:', chatId)
-        socket.join(chatId)
-      })
-
-      socket.on('disconnect', () => {
-        console.log('Client disconnected')
-      })
+    socket.on('disconnect', () => {
+      console.log('Client disconnected')
     })
+  })
 
-    res.socket.server.io = io
-  }
-  res.end()
+  global.io = io
+} else {
+  io = global.io
 }
 
-export const GET = initSocket
-export const POST = initSocket 
+export async function GET() {
+  return new NextResponse(null, { status: 200 })
+}
+
+export async function POST() {
+  return new NextResponse(null, { status: 200 })
+} 
