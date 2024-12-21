7e7ec944035f5dc611b715521d1245296bfd5869
lebonheurbb <122594742+lebonheurbb@users.noreply.github.com>
2024-12-21 16:08:51 +0800

    fix: 添加 bcryptjs 所需的依赖

--- next.config.js
+++ next.config.js
@@ -15,7 +15,16 @@ const nextConfig = {
         ],
       },
     ]
-  }
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      util: require.resolve('util/'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig 
