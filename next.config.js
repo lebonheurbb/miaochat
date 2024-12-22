6e9e89d5e72749e349c360ccbd0a1d794a4b3e56
lebonheurbb <122594742+lebonheurbb@users.noreply.github.com>
2024-12-23 00:51:24 +0800

    feat: add standalone output and experimental config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
   images: {
     remotePatterns: [
       {
